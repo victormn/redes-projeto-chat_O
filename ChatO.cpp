93db7040561da50a9276c37507324cf0cc90a3a7
Victor <victor95nunes@gmail.com>
2015-06-29 02:07:31 -0300

    colocando nome no código

--- ChatO.cpp
+++ ChatO.cpp
@@ -1,3 +1,11 @@
+//Projeto para a disciplina Redes de Computadores (SSC0641) da Universidade de São Paulo - Campus São Carlos - 2015
+//
+//Grupo 08
+//
+//Elisa Jorge Marcatto 7961965
+//Victor Marcelino Nunes 8622381
+
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
